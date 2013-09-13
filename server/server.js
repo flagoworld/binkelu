501c1f66e2f271381628f5e181ccb23f9967469a
Ryan <rlayne.ca@gmail.com>
2013-09-13 12:29:55 -0700

    reply to packets received

--- server/server.js
+++ server/server.js
@@ -9,10 +9,17 @@ server.on("error",function(err)
 server.on("message",function(msg,rinfo)
 {
 	console.log("Message: \n"+msg+" from "+rinfo.address+":"+rinfo.port);
+	
+	var msg=new Buffer("Gotcha!");
+	server.send(msg,0,msg.length,rinfo.port,rinfo.address,function(err,bytes)
+	{
+		console.log("Replied.");
+	});
 });
 
 server.on("listening",function()
 {
+	var address=server.address();
 	console.log("Listening: \n"+address.address+":"+address.port);
 });
 
